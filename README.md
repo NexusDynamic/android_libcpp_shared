79bddf11a1b2afe55c14cd8b32079df4a7743d85
zeyus <dev@zeyus.com>
2026-03-17 23:04:08 +0100

    Update library to support List<Uri> for system lib resolution.

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # android_libcpp_shared
 
-Add the shared Android C++ runtime to your Dart / Flutter app.
+Dart / flutter package for Android to add the libc++_shared.so STL C++ shared runtime library to your app
+
+# Usage
+
+Add the package to your pubspec.yaml dependencies:
+
+```yaml
+dependencies:
+  android_libcpp_shared: ^0.1.0
+```
+
+You don't need to import anything into your Dart code, the dependency is sufficient to bundle the native library with your app.
--- hook/build.dart
+++ hook/build.dart
@@ -4,36 +4,28 @@ import 'package:logging/logging.dart';
 import 'package:native_toolchain_c/native_toolchain_c.dart';
 
 void main(List<String> args) async {
+  final logger = Logger('AndroidLibcppSharedHook')
+    ..onRecord.listen((record) {
+      print('${record.level.name}: ${record.message}');
+    });
+
   await build(args, (input, output) async {
     if (!input.config.buildCodeAssets) {
-      print(
-        'buildCodeAssets is false, skipping Android system library inclusion.',
-      );
       return;
     }
     final targetOs = input.config.code.targetOS;
     if (targetOs != OS.android) {
-      print(
+      logger.info(
         'Target OS is $targetOs, skipping Android system library inclusion.',
       );
       return;
     }
-    print('Adding libc++_shared.so from Android system libraries...');
-    final temp = await resolveAndroidSystemLibPath(
-      input.config.code,
-      Logger('AndroidLibcppSharedHook')
-        ..onRecord.listen((record) {
-          print('${record.level.name}: ${record.message}');
-        }),
-    );
-    print('Resolved Android system library path: $temp');
+    logger.info('Adding libc++_shared.so from Android system libraries...');
+
     final androidLibDir = await resolveAndroidSystemLibPath(
       input.config.code,
-      Logger('AndroidLibcppSharedHook')
-        ..onRecord.listen((record) {
-          print('${record.level.name}: ${record.message}');
-        }),
-    );
+      logger: logger,
+    ).first;
     final libcppSharedPath = androidLibDir.resolve('libc++_shared.so');
     output.assets.code.add(
       CodeAsset(
