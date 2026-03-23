6dd363fe52adc5f8ecf1482e14e3f1aba63efb19
zeyus <dev@zeyus.com>
2026-03-23 09:41:47 +0100

    refactor NDK finder

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -25,27 +25,57 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late ffi.DynamicLibrary _lib;
+  late final ffi.DynamicLibrary? _lib;
+  int _randomNumber = -1;
 
   @override
   void initState() {
     super.initState();
+    _randomNumber = -1; // Initialize with an invalid random number.
     _lib = libCppShared;
   }
 
-  // Example of using a foreign function from libc++_shared.so.
-  // Generate a random number between 0 and 99 using the C standard library's rand() function.
-  int getRandomInt() {
-    final getTime = _lib
+  /// Example of using a foreign function from libc++_shared.so.
+  /// Generate a random number between 0 and 99 using the C standard library's
+  /// rand() function.
+  void getRandomInt() {
+    if (_lib == null) {
+      return;
+    }
+    final rand = _lib
         .lookup<ffi.NativeFunction<ffi.Int64 Function()>>('rand')
         .asFunction<int Function()>();
-    final time = getTime();
-    return time % 100;
+    final time = rand();
+    setState(() {
+      _randomNumber = time % 100; // Get a number between 0 and 99.
+    });
   }
 
   @override
   Widget build(BuildContext context) => Scaffold(
     appBar: AppBar(title: const Text('Android libc++_shared.so Example')),
-    body: Center(child: Text('${getRandomInt()}')),
+    body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            _lib != null
+                ? 'Random number from libc++_shared.so: $_randomNumber'
+                : 'libc++_shared.so is not available on this platform.',
+            textAlign: TextAlign.center,
+          ),
+          ElevatedButton(
+            onPressed: () => getRandomInt(),
+            child: const Text('Generate New Random Number'),
+          ),
+        ],
+      ),
+    ),
   );
+
+  @override
+  void dispose() {
+    _lib?.close();
+    super.dispose();
+  }
 }
